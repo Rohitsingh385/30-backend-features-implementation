8984ecb93ff771599c302cc276acb164cdccb043
unknown <rk301855@gmail.com>
2026-06-01 16:45:51 +0530

    fix: fixed the n +1 query with batch

--- nested-comments/src/features/comment.controller.ts
+++ nested-comments/src/features/comment.controller.ts
@@ -2,40 +2,40 @@ import type { Request, Response } from "express";
 import { commentModel } from "./comment.model.js";
 import { userModel } from "./user.model.js";
 import postModel from "./post.model.js";
-export const commentController = async(req: Request, res: Response)=> {
-    try{
-        
+export const commentController = async (req: Request, res: Response) => {
+    try {
+
         const { postId } = req.query as {
             postId: string
         }
-     
-        
-       const comments =  await commentModel.find({
+
+
+        const comments = await commentModel.find({
             postId: postId,
             parentComment: null
         })
-        .populate("author")
-        .populate("postId")
-       
+            .populate("author")
+            .populate("postId")
+
         const nestedComments: any[] = [];
 
-        for(const comment of comments){
+        for (const comment of comments) {
             const replies = await commentModel.find({
                 parentComment: comment._id
             })
-            .populate("author")
+                .populate("author")
 
             nestedComments.push({
                 ...comment.toObject(),
                 replies: replies
             })
         }
-        
+
         return res.status(200).json({
-            comments
+            comments: nestedComments
         })
 
-    }catch(err){
+    } catch (err) {
         console.log(err)
         return res.status(500).json({
             message: 'something went wrong'
@@ -43,15 +43,15 @@ export const commentController = async(req: Request, res: Response)=> {
     }
 }
 
-export const reply = async(req: Request, res: Response)=> {
-    try{
-        const { content, author, postId, parentComment} = req.body;
-        if(!parentComment){
+export const reply = async (req: Request, res: Response) => {
+    try {
+        const { content, author, postId, parentComment } = req.body;
+        if (!parentComment) {
             return res.json('invalid parent comment id')
         }
         const findComment = await commentModel.findById(parentComment)
-        if(!findComment){
-            return res.json({message: 'comment doesnt exists'})
+        if (!findComment) {
+            return res.json({ message: 'comment doesnt exists' })
         }
         const reply = await commentModel.create({
             content,
@@ -62,7 +62,7 @@ export const reply = async(req: Request, res: Response)=> {
         return res.status(200).json({
             message: 'reply added'
         })
-    }catch(err){
+    } catch (err) {
         console.log(err)
         return res.json({
             message: 'something went wrong'
@@ -70,9 +70,9 @@ export const reply = async(req: Request, res: Response)=> {
     }
 }
 
-export const getreply = async(req: Request, res: Response)=> {
-    try{
-        const {commentId } = req.query as {
+export const getreply = async (req: Request, res: Response) => {
+    try {
+        const { commentId } = req.query as {
             commentId: string
         }
         const replies = await commentModel.find({
@@ -82,8 +82,66 @@ export const getreply = async(req: Request, res: Response)=> {
         return res.status(200).json({
             replies
         })
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
+            message: 'something went wrong'
+        })
+    }
+}
+
+export const getBachData = async (req: Request, res: Response) => {
+    try {
+
+        const { postId } = req.query as {
+            postId: string
+        }
+
+
+        const comments = await commentModel.find({
+            postId: postId,
+            parentComment: null
+        })
+            .populate("author")
+            .populate("postId")
+
+        const commentIds = comments.map(comment => comment._id)
+        //console.log(commentIds);
+
+        const response = await commentModel.find({
+            parentComment: {
+                $in: commentIds
+            }
+        })
+
+        const groupedReplies: any = {};
+
+        for (const reply of response) {
+            const parentId = reply.parentComment.toString()
+            if (!groupedReplies[parentId]) {
+                groupedReplies[parentId] = []
+            }
+            groupedReplies[parentId].push(reply)
+
+        }
+
+        const nestedComments = [];
+
+        for (const comment of comments) {
+            const commentId = comment._id.toString()
+            nestedComments.push({
+                ...comment.toObject(),
+                replies: groupedReplies[commentId] || []
+            })
+        }
+        //console.log(response);
+
+        return res.status(200).json({
+            nestedComments
+        })
+
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({
             message: 'something went wrong'
         })
     }
