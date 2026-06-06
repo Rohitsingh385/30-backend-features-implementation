45619a606ee2e355602760cc69b22f84915556e4
unknown <rk301855@gmail.com>
2026-06-06 19:11:24 +0530

    wip: minor bugs fixes

--- follow-system/src/features/auth/user.controller.ts
+++ follow-system/src/features/auth/user.controller.ts
@@ -1,39 +1,48 @@
 import type { Request, Response } from "express"
-import jwt  from "jsonwebtoken"
+import jwt from "jsonwebtoken"
 import { signupSchema, loginSchema } from "./user.validation.js"
 import { signUpModel } from "./user.model.js"
+import bcrypt from "bcryptjs"
 const JWT_SECRET = 'donotStoreJwtLikeThis'
 export const signup = async (req: Request, res: Response) => {
 
-    const body = signupSchema.parse(req.body)
+    try {
+        const body = signupSchema.parse(req.body)
 
-    const { name, email, password } = body;
+        const { name, email, password } = body;
 
-    const checkExisting = await signUpModel.findOne({
-        email
-    })
-    if (checkExisting) {
-        return res.status(409).json({
-            message: 'user already exists'
+        const checkExisting = await signUpModel.findOne({
+            email
+        })
+        if (checkExisting) {
+            return res.status(409).json({
+                message: 'user already exists'
+            })
+        }
+         const hashPassword = bcrypt.hash(password, 10)
+        const newUser = await signUpModel.create({
+            name: name,
+            email: email,
+            password:
+        })
+
+        const token = jwt.sign({ email: newUser.email }, JWT_SECRET, { expiresIn: '1h' })
+
+        return res.status(200).json({
+            message: 'user registered',
+            token: token,
+            user: {
+                id: newUser._id,
+                name: newUser.name,
+                email: newUser.email
+            }
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'something went wrong'
         })
     }
-    const newUser = await signUpModel.create({
-        name,
-        email,
-        password
-    })
-
-    const token = jwt.sign({ email: newUser.email }, JWT_SECRET, { expiresIn: '1h' })
-
-    return res.status(200).json({
-        message: 'user registered',
-        token: token,
-        user: {
-            id: newUser._id,
-            name: newUser.name,
-            email: newUser.email
-        }
-    })
 
 
 }
@@ -43,7 +52,7 @@ export const login = async (req: Request, res: Response) => {
         const body = loginSchema.parse(req.body);
         const { email, password } = body;
 
-        const user = await signUpModel.find({ email })
+        const user = await signUpModel.findOne({ email })
 
         if (!user) {
             return res.status(401).json({
